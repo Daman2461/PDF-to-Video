97ab176412841d0c4fb5cc32023056b2505749ed
Damanjit Singh <142651012+Daman2461@users.noreply.github.com>
2025-02-23 12:09:25 +0530

    Update final.py

--- final.py
+++ final.py
@@ -17,7 +17,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.chains import create_retrieval_chain
 from elevenlabs.client import ElevenLabs
-from moviepy.editor import VideoFileClip, AudioFileClip, CompositeAudioClip, ImageClip, concatenate_videoclips
+from moviepy import VideoFileClip, AudioFileClip, CompositeAudioClip, ImageClip, concatenate_videoclips
 from PIL import Image
 from io import BytesIO
 st.set_page_config(
